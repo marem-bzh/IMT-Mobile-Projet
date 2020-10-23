a9c816525cb5e724b2c320472016effd5db98da3
Evrard-Nil Daillet <evrardnild@gmail.com>
2020-10-23 16:14:31 +0200

    init homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import {
+  IonButton,
+  IonButtons,
   IonContent,
   IonHeader,
+  IonImg,
+  IonMenuButton,
   IonPage,
   IonTitle,
   IonToolbar,
@@ -14,11 +18,29 @@ const Home: React.FC = () => {
     <IonPage id="home-page">
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Inbox</IonTitle>
+          <IonTitle>DevFest Nantes 2018</IonTitle>
+          <IonButtons slot="end">
+            <IonMenuButton autoHide={false} />
+          </IonButtons>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        HOME
+
+
+
+        <ul>
+          <li><span>
+            <IonImg src="https://upload.wikimedia.org/wikipedia/commons/6/67/IMT_Atlantique_logo.png" />
+          </span></li>
+          <li><span>
+            <h2>DevFest 2018</h2>
+              18th oct - 19th oct
+            </span></li>
+          <li><span>
+            <IonButton expand="block" >See speakers</IonButton>
+            <IonButton expand="block" >See talks</IonButton>
+          </span></li>
+        </ul>
       </IonContent>
     </IonPage>
   );
