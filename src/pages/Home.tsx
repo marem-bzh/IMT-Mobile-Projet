faeaad995b3b1ccceb0174bc410547376b094fd7
Evrard-Nil Daillet <evrardnild@gmail.com>
2020-10-23 15:23:44 +0200

    cleaned unused code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,33 +1,14 @@
-import SessionListItem from '../components/SessionListItem';
-import React, { useState } from 'react';
+import React from 'react';
 import {
   IonContent,
   IonHeader,
-  IonList,
   IonPage,
-  IonRefresher,
-  IonRefresherContent,
   IonTitle,
   IonToolbar,
-  useIonViewWillEnter
 } from '@ionic/react';
 import './Home.css';
-import { Session, getSessions } from '../data/sessions';
 
 const Home: React.FC = () => {
-
-  const [sessions, setSessions] = useState<Session[]>([]);
-
-  useIonViewWillEnter(() => {
-    const s = getSessions();
-    setSessions(s);
-  });
-
-  const refresh = (e: CustomEvent) => {
-    setTimeout(() => {
-      e.detail.complete();
-    }, 3000);
-  };
 
   return (
     <IonPage id="home-page">
@@ -37,21 +18,7 @@ const Home: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonRefresher slot="fixed" onIonRefresh={refresh}>
-          <IonRefresherContent></IonRefresherContent>
-        </IonRefresher>
-
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">
-              Inbox
-            </IonTitle>
-          </IonToolbar>
-        </IonHeader>
-
-        <IonList>
-          {sessions.map(m => <SessionListItem key={m.id} session={m} />)}
-        </IonList>
+        HOME
       </IonContent>
     </IonPage>
   );
