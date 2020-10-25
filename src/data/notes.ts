f2763ef4c9b2a0b2c703a6ef72a4235eb10c35f8
Evrard-Nil Daillet <evrardnild@gmail.com>
2020-10-25 12:07:34 +0100

    Store notes

--- src/data/notes.ts
+++ src/data/notes.ts
@@ -1,19 +1,34 @@
 
-var notes: { [id: number]: string; } = {
-};
+import { Plugins } from '@capacitor/core';
+
+const { Storage } = Plugins;
+
+_initNotes();
+
+var notes: { [id: number]: string; } = {};
 
 export const getNotes = () => notes;
-
+export const saveNote = _saveNote;
 export const getNote = (id: number) => notes[id];
 
-export const saveNote = _saveNote;
+async function _initNotes() {
+    console.log("Retrieving notes from storage");
+    const ret = await Storage.get({ key: 'notes' });
+    const value: string = ret.value ? ret.value : "{}";
+    notes = JSON.parse(value);
+    console.log("Note states:");
+    console.log(notes);
+}
 
-function _saveNote(id: number, content: string) {
+
+async function _saveNote(id: number, content: string) {
     const newNote = { [id]: content };
     notes = Object.assign({}, notes, newNote);
-
-    console.log("Saving new note: " + newNote);
-    console.log("Note states:" + notes);
+    await Storage.set({ key: 'notes', value: JSON.stringify(notes) });
+    console.log("Saving note: ");
+    console.log(newNote);
+    console.log("Note states:");
+    console.log(notes);
 }
 
 
