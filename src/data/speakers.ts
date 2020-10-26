e318b1a02b56477bf6d127ffb85c9d6dd6d3d041
Evrard-Nil Daillet <evrardnild@gmail.com>
2020-10-26 21:51:39 +0100

    Make sure speaker id is a number

--- src/data/speakers.ts
+++ src/data/speakers.ts
@@ -5,45 +5,43 @@ const SPEAKERS_PHOTOS_BASE_URL = "https://devfest2018.gdgnantes.com/";
 const SPEAKERS_STORAGE_KEY = "speakers";
 
 export interface Speaker {
-    id: number;
-    name: string;
-    photoUrl: string;
-    bio: string;
+  id: number;
+  name: string;
+  photoUrl: string;
+  bio: string;
 }
 
 export function fetchSpeakers() {
-    fetch(SPEAKERS_URL)
-        .then((response) => response.json())
-        .then((response) => {
-            response = Object.keys(response).map((key) => response[parseInt(key)]); // The API returns an object instead of an array so we're fixing that here
+  fetch(SPEAKERS_URL)
+    .then((response) => response.json())
+    .then((response) => {
+      response = Object.keys(response).map((key) => response[parseInt(key)]); // The API returns an object instead of an array so we're fixing that here
+      const speakers: Speaker[] = response.map((s: any) => {
+        return {
+          id: Number(s.id),
+          name: s.name,
+          photoUrl: SPEAKERS_PHOTOS_BASE_URL + s.photoUrl,
+          bio: s.shortBio,
+        };
+      });
 
-            const speakers: Speaker[] = response.map((s: any) => {
-                return {
-                    id: s.id,
-                    name: s.name,
-                    photoUrl: SPEAKERS_PHOTOS_BASE_URL + s.photoUrl,
-                    bio: s.shortBio,
-                };
-            });
-
-            return Storage.set({
-                key: SPEAKERS_STORAGE_KEY,
-                value: JSON.stringify(speakers),
-            });
-        })
-        .catch((error) => console.log(error));
+      return Storage.set({
+        key: SPEAKERS_STORAGE_KEY,
+        value: JSON.stringify(speakers),
+      });
+    })
+    .catch((error) => console.log(error));
 }
 
-export async function getSpeakers() {
-    const data = await Storage.get({ key: SPEAKERS_STORAGE_KEY });
-    if (data.value == null) return [];
+export async function getSpeakers(): Promise<Speaker[]> {
+  const data = await Storage.get({ key: SPEAKERS_STORAGE_KEY });
+  if (data.value == null) return [];
 
-    let speakers = JSON.parse(data.value);
-    return speakers;
+  let speakers = JSON.parse(data.value);
+  return speakers;
 }
 
-export async function getSpeaker(id: number): Promise<Speaker> {
-    const ret = (await getSpeakers()).find((s: Speaker) => s.id == id);
-    return ret;
+export async function getSpeaker(id: number): Promise<Speaker | undefined> {
+  const ret = (await getSpeakers()).find((s: Speaker) => s.id === id);
+  return ret;
 }
-
